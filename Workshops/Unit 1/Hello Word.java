7e63a8f06626771d041f671b3aeaa888d965a033
unknown <Evelin630>
2021-01-08 13:56:22 -0500

    workshops03operators

---
+++ Workshops/Unit 1/Hello Word.java
@@ -0,0 +1,11 @@
+// Evelyn Sanchez Hello World 
+
+/*
+some lines of comments
+can be written in this way
+thanks
+*/
+class HelloWord{ 
+    public static void main(String[]args){
+		system.out.println("Hello Word Evelyn Sanchez"):
+		
---
+++ Workshops/Unit 1/HelloWord.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+	//printf() display information
+	printf("Hello World from\t \" Evelyn Sanchez \" =) \n");
+	printf("\n The grade is %d  \n", grade);
+	return 0;
+}
