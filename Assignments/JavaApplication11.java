729bff41a8fc75518159a320ae547d5d35c7b58a
unknown <Evelin630>
2021-01-10 23:25:59 -0500

    2

---
+++ Assignments/JavaApplication11.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication11;
+
+/**
+ *
+ * @author Sony
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    long base;
+long height;
+long area;
+base = 17;
+height = 22;
+area = base*height;
+System.out.println("the area of" + base + "*" + "is equeal to" + area);
+}
+    
+}
---
+++ Assignments/JavaApplication9.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication9;
+
+/**
+ *
+ * @author Sony
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+    
+float base;
+float height;
+float area;
+base = 17;
+height = 22;
+area = base*height;
+System.out.println("the area of" + base + "*" + "is equeal to" + area);
+
+   
+   
+
+
+        
+            
+    
+}
