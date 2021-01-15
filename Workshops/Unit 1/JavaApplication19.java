4060d51218741355a6cb6071dae891b23aa1ee19
unknown <Evelin630>
2021-01-15 18:40:41 -0500

    workshops03operators

---
+++ Workshops/Unit 1/JavaApplication19.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication19;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sony
+ */
+public class JavaApplication19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+        Scanner input = new Scanner(System.in);
+        boolean mainLoop = true;
+        int option;
+
+        do {
+            System.out.println(" ========= Calculator =======");
+            System.out.println("1. -> Addition");
+            System.out.println("2. -> Subtraction");
+            System.out.println("3. -> Multiplication");
+            System.out.println("4. -> Division");
+            System.out.println("5. -> Exit");
+
+            System.out.println("Enter your menu option --> ");
+            option = input.nextInt();
+
+            switch (option) {
+
+                case 1:
+                    int addend1;
+                    int addend2;
+                    int sum;
+                    System.out.println("enter addend 1 -> ");
+                    addend1 = input.nextInt();
+                    System.out.println("enter addend 2 -> ");
+                    addend2 = input.nextInt();
+                    sum = addend1 + addend2;
+                    System.out.println(" the sum is --> " + sum);
+                    break;
+                case 2:
+                    System.out.println("The subtraction is");
+                    System.out.println("enter addend 2 -> ");
+                    addend1 = input.nextInt();
+                    System.out.println("enter addend 4 -> ");
+                    addend2 = input.nextInt();
+                    sum = addend1 + addend2;
+                    System.out.println(" the sum is --> " + sum);
+                    System.out.println("enter addend 6 -> ");
+                    addend1 = input.nextInt();
+                    System.out.println("enter addend  -> ");
+                    addend2 = input.nextInt();
+                    sum = addend1 + addend2;
+                    System.out.println(" the sum is --> " + sum);
+                    break;
+                case 3:
+                    System.out.println("The multiplcation is");
+                    System.out.println("enter addend 1 -> ");
+                    addend1 = input.nextInt();
+                    System.out.println("enter addend 8 -> ");
+                    addend2 = input.nextInt();
+                    sum = addend1 + addend2;
+                    System.out.println(" the sum is --> " + sum);
+                    System.out.println("enter addend 3 -> ");
+                    addend1 = input.nextInt();
+                    System.out.println("enter addend 2 -> ");
+                    addend2 = input.nextInt();
+                    sum = addend1 + addend2;
+                    System.out.println(" the sum is --> " + sum);
+                    break;
+                case 4:
+                    System.out.println("The division is");
+                    break;
+                case 5:
+                    System.out.println("Good Bye my friend");
+                    System.exit(0);
+                    break;
+             
+                   
+            }
+
+        
+
+ }
+    
+}
