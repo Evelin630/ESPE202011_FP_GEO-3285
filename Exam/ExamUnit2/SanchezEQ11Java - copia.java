e9c05e48f07917c0108404ac2e65192760f3b194
Evelin630 <Evelin630>
2021-03-08 22:27:38 -0500

    HW15

--- Exam/ExamUnit2/SanchezEQ11Java - copia.java
+++ Exam/ExamUnit2/SanchezEQ11Java - copia.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 /**
  *
- * @author So
+ * @author Sony
  */
 public class SanchezEQ11Java {
 
---
+++ Workshops/Unit 3/HW15.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw15;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sony
+ */
+public class HW15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Autor Evelyn Sanchez");
+        System.out.println(" ☀Hello☀ ");
+        System.out.println("My name is Evelyn");
+        System.out.println("-----Welcome to the calculator to Matrix Multiplication-----");
+       
+
+        int rows;
+        System.out.println("----Enter the rows of the arrays----");
+        rows = input.nextInt();
+        int columns;
+        System.out.println("----Enter the columns of the matrices----");
+        columns = input.nextInt();
+
+        int matrixA[][] = new int[rows][columns];;
+        int matrixB[][] = new int[rows][columns];;
+        int matrixMultiplication[][] = new int[rows][columns];
+     
+
+        System.out.println("Matrix A data:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(" the position: a[" + i + "][" + j + "] " + " --> ");
+                matrixA[i][j] = input.nextInt();
+
+            }
+
+        }
+
+        System.out.println("Matrix B data:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("the position: b[" + i + "][" + j + "] " + " --> ");
+                matrixB[i][j] = input.nextInt();
+
+            }
+        }
+        System.out.println();
+        System.out.println("Matrix A");
+        
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA[i].length; j++) {
+                System.out.print("\t " + matrixA[i][j]);
+            }
+            System.out.println();
+        }
+        
+        System.out.println();
+        System.out.println("Matrix B");
+        
+        for (int i = 0; i < matrixB.length; i++) {
+            for (int j = 0; j < matrixB[i].length; j++) {
+                System.out.print("\t " + matrixB[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
